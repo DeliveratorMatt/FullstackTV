46a5cb2a4cd450186bfd8ca9fa86afc382d76de9
Matt Weber <mathteacherone@gmail.com>
2025-03-11 21:01:13 -0400

    all coded not tested

--- src/shows/ShowDetails.jsx
+++ src/shows/ShowDetails.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import "./shows.css";
+import EpisodeList from "../EpisodeList.jsx";
+import EpisodeDetails from "../EpisodeDetails.jsx";
 
 /** Allows users to browse through the episodes of the given show */
 export default function ShowDetails({ show }) {
@@ -13,5 +15,16 @@ export default function ShowDetails({ show }) {
       </section>
     );
   }
-  return <div className="show-details"></div>;
+
+  return (
+    <div className="show-details">
+      <EpisodeList
+        name={show.name}
+        episodes={show.episodes}
+        selectedEpisode={selectedEpisode}
+        setSelectedEpisode={setSelectedEpisode}
+      />
+      <EpisodeDetails episode={selectedEpisode} />
+    </div>
+  );
 }
